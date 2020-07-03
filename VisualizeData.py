c75721fa3f8053bd82a2e700f58b3ffe7a710bbc
Nikhil Dandamudi <64459048+VenoSnake@users.noreply.github.com>
2020-07-03 09:07:10 +0530

    Update VisualizeData.py

--- VisualizeData.py
+++ VisualizeData.py
@@ -2,13 +2,14 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
-import numpy as np
+import statistics
 import ast
+import math
 
 def ParseRawData(ConfigFile):
 
     Count = 1
-    BulletsData = {1 : [], 2 : [], 3 : []}
+    BulletsData = {1 : [], 2 : []}
     File = open(ConfigFile, 'r')
     Lines = File.read().split('\n')
     for Line in Lines:
@@ -39,34 +40,67 @@ def ConvertPlotType(RelativeSet):
 def ScatterXVariation(Data):
 
     ModifData = {2 : [], 3 : [], 4 : [], 5 : [], 6 : [], 7 : [], 8 : [], 9 : [], 10 : []}
-    ConstantY = [0] * 4
+    ConstantY = [0] * 20
     for Bullet in range(2, 11):
         for val in Data:
             ModifData[Bullet].append(val[0][Bullet - 2])
+
+    '''
+    _MeanX = MeanMovement(ModifData)
 
     plt.figure(figsize = [10, 8])
     for i in range(9):
         plt.subplot(3, 3, i + 1)
         plt.scatter(ModifData[i + 2], ConstantY)
+        plt.plot([_MeanX[i], _MeanX[i]], [-0.04, 0.04], color = 'purple')
         plt.title('Bullet : {} - {}'.format(i + 1, i + 2), fontsize = 8)
         plt.yticks([])
+    '''
+
+    return ModifData
 
 def ScatterYVariation(Data):
 
     ModifData = {2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: []}
-    ConstantX = [0] * 4
+    ConstantX = [0] * 20
     for Bullet in range(2, 11):
         for val in Data:
             ModifData[Bullet].append(val[1][Bullet - 2])
+
+    '''
+    _MeanY = MeanMovement(ModifData)
 
     plt.figure(figsize=[8, 10])
     for i in range(9):
         plt.subplot(3, 3, i + 1)
         plt.scatter(ConstantX, ModifData[i + 2])
+        plt.plot([-0.05, 0.05], [_MeanY[i], _MeanY[i]], color = 'purple')
         plt.title('Bullet : {} - {}'.format(i + 1, i + 2), fontsize=8)
         plt.xticks([])
+    '''
 
+    return ModifData
+
+def MeanMovement(BulletData):
+
+    MeanMouseMovement = []
+    for Bullet in range(2, 11):
+        MeanMouseMovement.append(statistics.mean(BulletData[Bullet]))
+
+    return MeanMouseMovement
+
+MeanX = []
+MeanY = []
+MoveXDirec = []
+MoveYDirec = []
 ParsedData = ParseRawData('Vandal.txt')
-ScatterYVariation(ParsedData[1])
-ScatterXVariation(ParsedData[1])
-plt.show()
+for i in range(2):
+    MeanX.append(MeanMovement(ScatterXVariation(ParsedData[i + 1])))
+    MeanY.append(MeanMovement(ScatterYVariation(ParsedData[i + 1])))
+for i in range(9):
+    MoveXDirec.append(math.ceil((MeanX[0][i] + MeanX[1][i]) / 2))
+    MoveYDirec.append(math.ceil((MeanY[0][i] + MeanY[1][i]) / 2))
+print()
+for i in range(9):
+    print('(', -1 * MoveXDirec[i], ',', -1 * MoveYDirec[i], ')', end=' ')
+print()
